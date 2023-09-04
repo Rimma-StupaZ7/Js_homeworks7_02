78f9442f33b3263f932ca470aa3cccb0eb928298
valentin stupa <valentin.stupa@ediweb.com>
2023-09-04 12:18:55 +0300

    homework done

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        'no-var': 'error',
+        semi: 'error',
+        indent: 'error',
+        'no-multi-spaces': 'error',
+        'space-in-parens': 'error',
+        'no-multiple-empty-lines': 'error',
+        'prefer-const': 'error',
+        'no-use-before-define': 'error',
+    },
+};
